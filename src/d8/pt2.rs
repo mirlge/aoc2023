7affe0639ab88dfcb5474a872f7c996b45970525
MineItRiGrEight <80468882+MineItRiGrEight@users.noreply.github.com>
2024-01-01 21:30:12 +0100

    feat: Do more work on day 8 part 2 (still not done)

--- src/d8/pt2.rs
+++ src/d8/pt2.rs
@@ -15,26 +15,40 @@ pub fn calculate_answer(input: String) -> i64 {
         acc.insert(x_vec[0], next_paths);
         acc
     });
-    let mut current_locations: Vec<&str> = paths.keys().filter(|&&x| x.ends_with("Z")).collect();
+    let mut current_locations: Vec<&str> = paths
+        .clone()
+        .into_keys()
+        .filter(|x| x.ends_with("A"))
+        .collect();
     let mut turns = 0;
     let mut lr_instruction_idx = 0;
     let lr_instructions_vec: Vec<_> = input_vec[0].chars().collect();
     //println!("[DEBUG] lr_instructions_vec: {:?}", lr_instructions_vec);
     let mut lr_instruction = lr_instructions_vec[lr_instruction_idx];
-    let mut done_locations = 0;
-    while done_locations < current_locations.len() {
-        //println!("  [DEBUG] current_location: {}", current_location);
+    let mut done_nodes = 0;
+    while done_nodes < current_locations.len() {
+        println!("  [DEBUG] current_locations: {:?}", current_locations);
         //println!("  [DEBUG] turns: {}", turns);
         //println!("  [DEBUG] lr_instruction_idx: {}", lr_instruction_idx);
         //println!("  [DEBUG] lr_instruction: {}", lr_instruction);
-        match lr_instruction {
-            'L' => current_location = &paths[current_location][0],
-            'R' => current_location = &paths[current_location][1],
-            _ => panic!(
-                "The current left/right instruction ({}) isn't 'L' or 'R'!",
-                lr_instruction
-            ),
-        }
+        done_nodes = 0;
+        current_locations = current_locations
+            .into_iter()
+            .map(|x| {
+                let new_x = match lr_instruction {
+                    'L' => paths[x][0].as_str(),
+                    'R' => paths[x][1].as_str(),
+                    _ => panic!(
+                        "The current left/right instruction ({}) isn't 'L' or 'R'!",
+                        lr_instruction
+                    ),
+                };
+                if new_x.ends_with("Z") {
+                    done_nodes += 1;
+                }
+                new_x
+            })
+            .collect();
         turns += 1;
         lr_instruction_idx = (lr_instruction_idx + 1) % lr_instructions_vec.len();
         lr_instruction = lr_instructions_vec[lr_instruction_idx];
